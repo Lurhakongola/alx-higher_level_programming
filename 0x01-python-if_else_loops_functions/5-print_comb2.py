efe7c83de0ccd92f82a0af180962e0b885a9dfaf
lurhakongola <alfredbalazirelurhakongola@gmail.com>
2023-08-20 22:26:27 +0300

    save changes of print numbers(99)

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for number in range(0, 100):
- if number == 99:
-    print("{}".format(number))
+    if number == 99:
+        print("{}".format(number))
 
 else:
-    print("{:02}".format(number). end=", ")
+    print("{0:02d}, ".format(number), end="")
